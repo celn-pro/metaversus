288935767b9c08778e78667763e69e6a86c34de4
celn-pro <ayouberasto7@gmail.com>
2023-08-25 12:04:15 +0300

    initial commit

---
+++ components/InsightCard.jsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { motion } from "framer-motion";
+
+import { fadeIn } from "../utils/motion";
+
+const InsightCard = ({ imgUrl, title, subtitle, index }) => (
+  <motion.div
+    variants={fadeIn("up", "spring", index * 0.5, 1)}
+    className="flex md:flex-row flex-col gap-4"
+  >
+    <img
+      src={imgUrl}
+      alt="planet-01"
+      className="md:w-[270px] w-full h-[250px] rounded-[32px] object-cover"
+    />
+    <div className="flex justify-between items-center w-full">
+
+    </div>
+  </motion.div>
+);
+
+export default InsightCard;
